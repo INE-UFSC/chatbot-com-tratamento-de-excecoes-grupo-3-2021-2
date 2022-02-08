706709aa9364b79f462cb2287078558f428d1d66
Thiago Bewiahn <thiagoaugustobewiahn@gmail.com>
2022-02-08 11:13:54 -0300

    Update SistemaChatBot.py
    Algumas adaptaçoes no sistema

--- SistemaChatBot/SistemaChatBot.py
+++ SistemaChatBot/SistemaChatBot.py
@@ -1,13 +1,15 @@
 from Bots.Bot import Bot
 
+
 class SistemaChatBot:
-    def __init__(self,nomeEmpresa,lista_bots):
+    def __init__(self, nomeEmpresa, lista_bots):
+        ehBot = True
         for i in lista_bots:
-            if isinstance(i,Bot):
-                ehBot = True
-            else:
+            try:
+                if isinstance(i, Bot) == False:
+                    raise TypeError
+            except TypeError:
                 ehBot = False
-                break
 
         self.__empresa = nomeEmpresa
         if ehBot:
@@ -15,23 +17,25 @@ class SistemaChatBot:
             self.__bot = None
         else:
             raise print('Erro! Nem todos os elementos são bots!')
-    
+
     def boas_vindas(self):
         print(f'Sejam bem vindos à imersão de bots da {self.__empresa}!')
 
     def mostra_menu(self):
         print('Os chat bots disponíveis no momento são:')
         for i in range(len(self.__lista_bots)):
-            print(f'{i+1} - Bot: {self.__lista_bots[i].nome} - Mensagem de apresentação: {self.__lista_bots[i].apresentacao()}')
+            print(
+                f'{i+1} - Bot: {self.__lista_bots[i].nome} - Mensagem de apresentação: {self.__lista_bots[i].apresentacao()}')
 
     def escolhe_bot(self):
 
-        entrada = int(input('Digite o número do chat bot desejado:(-1 para encerrar o programa)  '))
+        entrada = int(
+            input('Digite o número do chat bot desejado:(-1 para encerrar o programa)  '))
         if entrada == -1:
             print('Programa encerrado!')
             return True
         else:
-            while entrada < 0 or entrada > len(self.__lista_bots) :
+            while entrada < 0 or entrada > len(self.__lista_bots):
                 print('Valor inválido!')
                 entrada = int(input('Digite o número do chat bot desejado: '))
 
@@ -39,42 +43,40 @@ class SistemaChatBot:
             return False
 
     def mostra_comandos_bot(self):
-        bot_c = self.__bot.mostra_comandos()
-        index = 0
-        for i in bot_c:
-            print(f'{index} - {i}')
-            index += 1
+        print(self.__bot.mostra_comandos())
 
     def le_envia_comando(self):
-        entrada = int(input('Digite o comando desejado:(-1 para encerrar o programa) '))
+        entrada = int(
+            input('Digite o comando desejado:(-1 para encerrar o programa) '))
         if entrada == -1:
             return True
 
         while entrada < -1 or entrada >= len(self.__bot.mostra_comandos()):
             print('Valor inválido!')
-            entrada = int(input('Digite o comando desejado:(-1 para encerrar o programa) '))
+            entrada = int(
+                input('Digite o comando desejado:(-1 para encerrar o programa) '))
 
-        list_temp = []
-        dic = self.__bot.mostra_comandos()
-        for el in dic:
-            list_temp.append(dic[el])
+        try:
+            resposta_obj = self.__bot.executa_comando(entrada)
+            print(f'{self.__bot.nome} diz: {resposta_obj.getRandomResposta()}')
+        except KeyError:
+            print("Indíce não existe")
 
-        print(f'{self.__bot.nome} diz: {list_temp[entrada]}')
         return False
 
     def inicio(self):
-        ##mostra mensagem de boas-vindas do sistema
+        # mostra mensagem de boas-vindas do sistema
         self.boas_vindas()
-        ##mostra o menu ao usuário
+        # mostra o menu ao usuário
         self.mostra_menu()
-        ##escolha do bot
+        # escolha do bot
         check = self.escolhe_bot()
         if check:
             return print("Tchau!")
-        ##mostra mensagens de boas-vindas do bot escolhido
+        # mostra mensagens de boas-vindas do bot escolhido
         print(self.__bot.boas_vindas())
-        ##entra no loop de mostrar comandos do bot e escolher comando do bot até o usuário definir a saída
-        ##ao sair mostrar a mensagem de despedida do bot
+        # entra no loop de mostrar comandos do bot e escolher comando do bot até o usuário definir a saída
+        # ao sair mostrar a mensagem de despedida do bot
         while True:
             self.mostra_comandos_bot()
             end = self.le_envia_comando()
