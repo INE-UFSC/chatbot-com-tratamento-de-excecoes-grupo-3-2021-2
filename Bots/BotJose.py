ff3f0e73200987e292c43b6acf730001c7720cda
kalleoOuriques <90047255+kalleoOuriques@users.noreply.github.com>
2022-02-08 11:12:24 -0300

    Update BotJose.py

--- Bots/BotJose.py
+++ Bots/BotJose.py
@@ -3,11 +3,11 @@ from Bots.Bot import Bot
 class BotJose(Bot):
     def __init__(self,nome):
         self.__nome = nome
-        self.__comandos = {
-            "conselho para os estudos": "José analisa suas nota\nJosé diz: Desistir é para os fracos, o ideal é nem tentar",
-            "conselho amoroso": "José analisa seu Tinder\nJosé diz: Nunca é tarde para um novo fracasso",
-            "conselho para a carreira": "José te entrega um guia de como se comportar numa entrevista\nRegra 1: chame o empregador de 'meu parça', é contrato na certa",
-            "adeus": "José diz: Vamos esquecer os erros do passado, meu amigo, e focar nos erros do futuro. Adeus, até vista"}
+        super().__comandos = {
+            1: Comando(1, "conselho para os estudos", ["José analisa suas nota\nJosé diz: Desistir é para os fracos, o ideal é nem tentar"],
+            2: Comando(2, "conselho amoroso",["José analisa seu Tinder\nJosé diz: Nunca é tarde para um novo fracasso"],
+            3: Comando(3, "conselho para a carreira", ["José te entrega um guia de como se comportar numa entrevista\nRegra 1: chame o empregador de 'meu parça', é contrato na certa"],
+            4: Comando(4, "adeus", ["José diz: Vamos esquecer os erros do passado, meu amigo, e focar nos erros do futuro. Adeus, até vista"]}
 
     @property
     def nome(self):
@@ -24,8 +24,7 @@ class BotJose(Bot):
     def apresentacao(self):
         msg = f'Mensagem de apresentação: Olá, eu sou o {self.__nome}, seu bot conselheiro'
         return msg
-    def mostra_comandos(self):
-        return self.__comandos
+    
     
     def executa_comando(self,cmd):
         try:
